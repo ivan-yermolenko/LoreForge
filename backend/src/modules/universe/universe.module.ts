068ad5c8e82612a2037ad36e829440cf7726804b
Ivan <nikk452@gmail.com>
2026-06-08 10:00:50 +0300

    Integrate Characters, Worlds, and Tags services, repositories, and controllers into Universe module

--- backend/src/modules/universe/universe.module.ts
+++ backend/src/modules/universe/universe.module.ts
@@ -1,14 +1,41 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+import { World } from './worlds/entities/world.entity';
+import { Character } from './characters/entities/character.entity';
+import { Tag } from './tags/entities/tag.entity';
+
+import { UniverseService } from './universe.service';
 import { WorldsService } from './worlds/worlds.service';
+import { CharactersService } from './characters/characters.service';
+import { TagsService } from './tags/tags.service';
+
+import { WorldsRepository } from './worlds/worlds.repository';
+import { CharactersRepository } from './characters/characters.repository';
+import { TagsRepository } from './tags/tags.repository';
+
 import { WorldsController } from './worlds/worlds.controller';
-import { UniverseService } from "./universe.service";
-import { TypeOrmModule } from "@nestjs/typeorm";
-import { World } from "./worlds/entities/world.entity";
+import { CharactersController } from './characters/characters.controller';
+import { TagsController } from './tags/tags.controller';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([World])],
-  controllers: [WorldsController],
-  providers: [WorldsService, UniverseService],
+  imports: [
+    TypeOrmModule.forFeature([World, Character, Tag]),
+  ],
+  controllers: [
+    WorldsController,
+    CharactersController,
+    TagsController,
+  ],
+  providers: [
+    UniverseService,
+    WorldsService,
+    CharactersService,
+    TagsService,
+    WorldsRepository,
+    CharactersRepository,
+    TagsRepository,
+  ],
   exports: [UniverseService],
 })
 export class UniverseModule {}
--- backend/src/modules/universe/universe.service.ts
+++ backend/src/modules/universe/universe.service.ts
@@ -1,4 +1,19 @@
 import { Injectable } from '@nestjs/common';
+import { WorldsService } from './worlds/worlds.service';
+import { CharactersService } from './characters/characters.service';
+import { TagsService } from './tags/tags.service';
+
+export interface NarrativeContextValidationResult {
+  isValid: boolean;
+  reason?: 'WORLD_NOT_FOUND' | 'CHARACTERS_NOT_FOUND' | 'CHARACTER_WORLD_MISMATCH';
+  details?: string;
+}
 
 @Injectable()
-export class UniverseService {}
+export class UniverseService {
+  constructor(
+    private readonly worldsService: WorldsService,
+    private readonly charactersService: CharactersService,
+    private readonly tagsService: TagsService,
+  ) {}
+}
